57be9e0e512aa3cf064074983cd6cc2887d0ac47
Mariabayonae <bayonam346@gmail.com>
2026-05-04 16:27:17 +0000

    primeros lugares

---
+++ script.js
@@ -0,0 +1,19 @@
+const places = [
+{
+title:"Giethoorn",
+img:"https://images.unsplash.com/photo-1500375592092-40eb2168fd21",
+desc:"Pueblo sin carreteras donde todo se mueve en canales.",
+color:"#dbeafe"
+},
+{
+title:"Chefchaouen",
+img:"https://images.unsplash.com/photo-1524492412937-b28074a5d7da",
+desc:"Ciudad azul en Marruecos.",
+color:"#c7d2fe"
+},
+{
+title:"Socotra",
+img:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+desc:"Paisajes únicos del mundo.",
+color:"#fde68a"
+},
