6d1c73787d608abe821f623d937a44ac4979b38f
Mariabayonae <bayonam346@gmail.com>
2026-05-11 01:35:59 +0000

    ultimos cambios de la noche

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const postcards = document.querySelectorAll(".postcard");
-postcards.forEach(postcard => {
-postcard.addEventListener("click", () => {
-postcard.classList.toggle("flipped");
+postcards.forEach(card => {
+card.addEventListener("click", () => {
+card.classList.toggle("flipped");
   });
 });
